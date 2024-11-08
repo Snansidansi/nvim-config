faad0bbc1b4a1b9de3e2907bea4c3b1486ac3365
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2024-11-08 10:29:10 +0100

    Changed jdtls workspace to ~/code/workspace to match the eclipse generated files

--- lua/snansidansi/jdtls.lua
+++ lua/snansidansi/jdtls.lua
@@ -39,7 +39,7 @@ local function get_workspace()
 	-- Get the home directory of your operating system
 	local home = os.getenv("HOME")
 	-- Declare a directory where you would like to store project information
-	local workspace_path = home .. "/dev/.jdtls_workspace/"
+	local workspace_path = home .. "/code/workspace/"
 	-- Determine the project name
 	local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
 	-- Create the workspace directory by concatenating the designated workspace path and the project name
