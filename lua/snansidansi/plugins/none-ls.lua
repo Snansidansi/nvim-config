7be5d8d138c301dc0eae460765364711e5eee4e9
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2024-11-22 12:43:14 +0100

    Added format on save for go with lsp and made a formatting priority for null-ls over lsp

--- lua/snansidansi/plugins/none-ls.lua
+++ lua/snansidansi/plugins/none-ls.lua
@@ -8,12 +8,22 @@ return {
 
 		config = function()
 			local null_ls = require("null-ls")
-
 			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.formatting.google_java_format,
-				},
+
+            local lsp_formatting = function(bufnr)
+                vim.lsp.buf.format({
+                    filter = function(client)
+                        return client.name == "null-ls"
+                    end,
+                    bufnr = bufnr,
+                })
+            end
+
+            null_ls.setup({
+                sources = {
+                    null_ls.builtins.formatting.google_java_format,
+                },
+
 				on_attach = function(client, bufnr)
 					if client.supports_method("textDocument/formatting") then
 						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
@@ -21,9 +31,7 @@ return {
 							group = augroup,
 							buffer = bufnr,
 							callback = function()
-								-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-								-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
-								vim.lsp.buf.format({ async = false })
+                                lsp_formatting(bufnr)
 							end,
 						})
 					end
