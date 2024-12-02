3fc4b5c363c11ec3a6186c6f045fc0be4e6b22cc
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2024-12-02 22:49:30 +0100

    Added checkstyle for java

--- lua/snansidansi/plugins/none-ls.lua
+++ lua/snansidansi/plugins/none-ls.lua
@@ -22,6 +22,9 @@ return {
             null_ls.setup({
                 sources = {
                     null_ls.builtins.formatting.google_java_format,
+                    null_ls.builtins.diagnostics.checkstyle.with({
+                        extra_args = { "-c", "/google_checks.xml" }, -- or "/sun_checks.xml" or path to self written rules
+                    }),
                 },
 
 				on_attach = function(client, bufnr)
