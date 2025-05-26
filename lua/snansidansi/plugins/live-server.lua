9f445a8cae8cb29f185c4cdd917773205cf1ffe7
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2025-05-26 12:44:40 +0200

    Added npm live server

---
+++ lua/snansidansi/plugins/live-server.lua
@@ -0,0 +1,17 @@
+return {
+    --  run `npm install -g live-server`
+    'barrett-ruth/live-server.nvim',
+    build = "npm install -g live-server",
+
+    config = function()
+        require("live-server").setup({
+            args = { "--browser=explorer.exe" }
+        })
+
+    end,
+
+    cmd = { "LiveServerStart", "LiveServerStop" },
+
+    vim.keymap.set("n", "<leader>ls", "<cmd>LiveServerStart<CR>"),
+    vim.keymap.set("n", "<leader>lS", "<cmd>LiveServerStop<CR>"),
+}
