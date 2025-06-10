a85f6a13f223aeea00aa8dc138030e88d50a7168
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2025-06-10 12:36:50 +0200

    changed tabstop for .ts files

--- lua/snansidansi/autocommands.lua
+++ lua/snansidansi/autocommands.lua
@@ -31,7 +31,7 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "html", "js", "css" },
+	pattern = { "html", "js", "css", "ts" },
 	callback = function()
 		vim.opt_local.tabstop = 2
 		vim.opt_local.softtabstop = 2
