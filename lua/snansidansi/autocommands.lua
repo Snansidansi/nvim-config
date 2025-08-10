a2b0729816e3a5f4ea95046ba8083666b69b8dbd
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2025-08-10 11:26:58 +0200

    changed tabstop for jsx and tsx

--- lua/snansidansi/autocommands.lua
+++ lua/snansidansi/autocommands.lua
@@ -21,7 +21,7 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "html", "js", "css", "ts", "c", "cpp", "java" },
+	pattern = { "html", "js", "css", "ts", "c", "cpp", "java", "typescriptreact", "javascriptreact" },
 	callback = function()
 		vim.opt_local.tabstop = 2
 		vim.opt_local.softtabstop = 2
