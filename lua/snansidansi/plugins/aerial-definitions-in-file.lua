9cb181d767557cf7cee92d156ac15e17a6fa56b4
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2025-11-02 21:55:16 +0100

    Added aerial and treesitter-context

---
+++ lua/snansidansi/plugins/aerial-definitions-in-file.lua
@@ -0,0 +1,21 @@
+return {
+	"stevearc/aerial.nvim",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons",
+	},
+
+	config = function()
+		require("aerial").setup({
+			on_attach = function(bufnr)
+				vim.keymap.set("n", "ap", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+				vim.keymap.set("n", "an", "<cmd>AerialNext<CR>", { buffer = bufnr })
+			end,
+
+			layout = {
+				default_direction = "left",
+			},
+		})
+		vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle<CR>")
+	end,
+}
--- lua/snansidansi/plugins/telescope.lua
+++ lua/snansidansi/plugins/telescope.lua
@@ -19,7 +19,28 @@ return {
 					},
 					file_ignore_patterns = { "^target/", "node_modules/" },
 				},
+
+				extensions = {
+					aerial = {
+						-- Set the width of the first two columns (the second
+						-- is relevant only when show_columns is set to 'both')
+						col1_width = 4,
+						col2_width = 30,
+						-- How to format the symbols
+						format_symbol = function(symbol_path, filetype)
+							if filetype == "json" or filetype == "yaml" then
+								return table.concat(symbol_path, ".")
+							else
+								return symbol_path[#symbol_path]
+							end
+						end,
+						-- Available modes: symbols, lines, both
+						show_columns = "both",
+					},
+				},
 			})
+
+			vim.keymap.set("n", "<leader>pa", "<cmd>Telescope aerial<CR>")
 
 			local builtin = require("telescope.builtin")
 
