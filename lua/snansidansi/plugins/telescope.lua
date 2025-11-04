bf1b4097e42e9c18590e963378ebf18d93c0c12f
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2025-11-04 15:22:52 +0100

    fixed aerial telescope extension

--- lua/snansidansi/plugins/telescope.lua
+++ lua/snansidansi/plugins/telescope.lua
@@ -1,75 +1,69 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.8",
 
-		dependencies = { "nvim-lua/plenary.nvim" },
+        dependencies = { "nvim-lua/plenary.nvim" },
 
-		config = function()
-			require("telescope").setup({
-				defaults = {
-					layout_config = {
-						vertical = {
-							width = 0.7,
-							height = 0.7,
-							prompt_position = "bottom",
-							preview_height = 0.5,
-							preview_cutoff = 0,
-						},
-					},
-					file_ignore_patterns = { "^target/", "node_modules/" },
-				},
+        config = function()
+            require("telescope").setup({
+                defaults = {
+                    layout_config = {
+                        vertical = {
+                            width = 0.7,
+                            height = 0.7,
+                            prompt_position = "bottom",
+                            preview_height = 0.5,
+                            preview_cutoff = 0,
+                        },
+                    },
+                    file_ignore_patterns = { "^target/", "node_modules/" },
+                },
 
-				extensions = {
-					aerial = {
-						-- Set the width of the first two columns (the second
-						-- is relevant only when show_columns is set to 'both')
-						col1_width = 4,
-						col2_width = 30,
-						-- How to format the symbols
-						format_symbol = function(symbol_path, filetype)
-							if filetype == "json" or filetype == "yaml" then
-								return table.concat(symbol_path, ".")
-							else
-								return symbol_path[#symbol_path]
-							end
-						end,
-						-- Available modes: symbols, lines, both
-						show_columns = "both",
-					},
-				},
-			})
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown({}),
+                    },
+                    aerial = {
+                        -- Set the width of the first two columns (the second
+                        -- is relevant only when show_columns is set to 'both')
+                        col1_width = 2,
+                        -- col2_width = 30,
+                        -- How to format the symbols
+                        format_symbol = function(symbol_path, filetype)
+                            if filetype == "json" or filetype == "yaml" then
+                                return table.concat(symbol_path, ".")
+                            else
+                                return symbol_path[#symbol_path]
+                            end
+                        end,
+                        -- Available modes: symbols, lines, both
+                        show_columns = "symbols",
+                    },
+                },
+            })
+            require("telescope").load_extension("ui-select")
+            require('telescope').load_extension('aerial')
 
-			vim.keymap.set("n", "<leader>pa", "<cmd>Telescope aerial<CR>")
+            vim.keymap.set("n", "<leader>pa", "<cmd>Telescope aerial<CR>")
 
-			local builtin = require("telescope.builtin")
+            local builtin = require("telescope.builtin")
 
-			vim.keymap.set("n", "<leader>pf", function()
-				builtin.find_files({ layout_strategy = "vertical" })
-			end)
-			vim.keymap.set("n", "<leader>pg", function()
-				builtin.live_grep({ layout_strategy = "vertical" })
-			end)
-			vim.keymap.set("n", "<leader>gf", function()
-				builtin.git_files({ layout_strategy = "vertical" })
-			end)
-			vim.keymap.set("n", "<leader>lf", function()
-				builtin.resume({ layout_strategy = "vertical" })
-			end)
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-				require("telescope").load_extension("ui-select"),
-			})
-		end,
-	},
+            vim.keymap.set("n", "<leader>pf", function()
+                builtin.find_files({ layout_strategy = "vertical" })
+            end)
+            vim.keymap.set("n", "<leader>pg", function()
+                builtin.live_grep({ layout_strategy = "vertical" })
+            end)
+            vim.keymap.set("n", "<leader>gf", function()
+                builtin.git_files({ layout_strategy = "vertical" })
+            end)
+            vim.keymap.set("n", "<leader>lf", function()
+                builtin.resume({ layout_strategy = "vertical" })
+            end)
+        end,
+    },
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+    },
 }
