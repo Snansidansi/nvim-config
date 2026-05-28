407d8dd0c21b7583e14027551fdad4d867b50b1c
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2026-05-28 02:19:12 +0200

    Updated Telescope and Tresitter for nvim version >0.12.0
    chore: update lazy.nvim plugins and remove treesitter-context

--- lua/snansidansi/plugins/treesitter.lua
+++ lua/snansidansi/plugins/treesitter.lua
@@ -1,27 +1,10 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    build = ':TSUpdate',
 
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "c", "lua", "python", "markdown", "go", "java" },
-
-			-- Install parsers synchronously (only applied to `ensure_installed`)
-			sync_install = false,
-
-			-- Automatically install missing parsers when entering buffer
-			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-			auto_install = true,
-
-			highlight = {
-				enable = true,
-
-				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-				-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-				-- Using this option may slow down your editor, and you may see some duplicate highlights.
-				-- Instead of true it can also be a list of languages
-				additional_vim_regex_highlighting = false,
-			},
-		})
-	end,
+    require('nvim-treesitter').setup {
+        -- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
+            install_dir = vim.fn.stdpath('data') .. '/site'
+    }
 }
